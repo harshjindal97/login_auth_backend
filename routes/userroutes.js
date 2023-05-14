36e0b38d24e0d22cd3349ff5e7d47d917d97995c
harshjindal97 <hasrh946643@gmail.com>
2023-05-14 12:47:57 +0530

    testing commit message

---
+++ routes/userroutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const router = express.Router();
+import UserController from '../controller/usercontroller';
+
+// public router
+
+
+
+
+// private router
+
+//new commit
+
+export default router;
